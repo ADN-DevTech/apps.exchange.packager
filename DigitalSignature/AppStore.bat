0f7be2e4437d950d3ee044faa4e61ce7bf2a0193
cyrillef <cyrillef@free.fr>
2015-05-01 13:03:33 +0200

    Initial Release

---
+++ DigitalSignature/AppStore.bat
@@ -0,0 +1,9 @@
+rem @echo off
+set Certificate_Password=
+set Certificate_AppStore=
+
+rem ** Uncomment the 2 following line to use a certificate to sign the MSI installer
+rem ** the certificate should be located in this directory
+
+rem set Certificate_Password=pwd
+rem set Certificate_AppStore=AppStore.pfx
